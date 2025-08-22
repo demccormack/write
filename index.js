5e001a2b8b2a32f9949ddfae34f237af7850fde1
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2025-08-23 06:02:40 +1200

    Create package.json

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+console.log("Welcome to write!");
+console.log("This is a placeholder for the write application.");
+
+// Export something for programmatic use
+module.exports = {
+  version: require("./package.json").version,
+  name: "write",
+};
