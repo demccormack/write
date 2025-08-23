51ba2d5d02cb600c1cbbcd9dcf44dd148b0358e5
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2025-08-23 14:47:30 +1200

    Setup Prettier

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-console.log("Welcome to write!");
-console.log("This is a placeholder for the write application.");
+console.log('Welcome to write!');
+console.log('This is a placeholder for the write application.');
 
 // Export something for programmatic use
 module.exports = {
-  version: require("./package.json").version,
-  name: "write",
+  version: require('./package.json').version,
+  name: 'write',
 };
